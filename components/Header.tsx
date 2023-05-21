6fa71ef05aa3c77bf5e5c24ec869d04b45f54912
Vivek Patel <vivek.p9737@gmail.com>
2023-05-21 14:06:38 -0700

    changed domain info

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,62 +1,77 @@
-import React, {useContext, useEffect, useState} from 'react'
-import Link from 'next/link'
-import AppContext from '../contexts/AppContext';
+import React, { useContext, useEffect, useState } from "react";
+import Link from "next/link";
+import AppContext from "../contexts/AppContext";
 
 const Header = () => {
   const appState = useContext(AppContext);
-  const [themeMode, setThemeMode] = useState<'light' | 'dark'>('light');
-  
-  const {loader, meetActivate} = appState.state;
+  const [themeMode, setThemeMode] = useState<"light" | "dark">("light");
+
+  const { loader, meetActivate } = appState.state;
 
   useEffect(() => {
-    const oldThemeMode = localStorage.getItem('themeMode');
-    if(oldThemeMode) setThemeMode(oldThemeMode as 'light' | 'dark');
-    else{
-      const html = document.querySelector('html');
-      if(html) {
-        setThemeMode(html.classList.contains('dark') ? 'dark' : 'light');
+    const oldThemeMode = localStorage.getItem("themeMode");
+    if (oldThemeMode) setThemeMode(oldThemeMode as "light" | "dark");
+    else {
+      const html = document.querySelector("html");
+      if (html) {
+        setThemeMode(html.classList.contains("dark") ? "dark" : "light");
       }
     }
-  },[])
+  }, []);
 
   useEffect(() => {
-    const html = document.querySelector('html');
-    if(html) {
-      if(themeMode === 'dark') html.classList.add('dark');
-      else html.classList.remove('dark');
+    const html = document.querySelector("html");
+    if (html) {
+      if (themeMode === "dark") html.classList.add("dark");
+      else html.classList.remove("dark");
     }
-    localStorage.setItem('themeMode', themeMode);
-  },[themeMode])
+    localStorage.setItem("themeMode", themeMode);
+  }, [themeMode]);
 
   const toggleThemeMode = () => {
-      setThemeMode(themeMode === 'dark' ? 'light' : 'dark');
-  }
+    setThemeMode(themeMode === "dark" ? "light" : "dark");
+  };
 
   return (
-    <div className='sticky z-50 top-0 left-0 transition-none dark:text-white '>
-     <div className="mx-20 px-6 py-4 flex justify-between">
-      <Link href='/'>
-          <div className={`font-semibold text-xl no-underline text-center w-20 transition ease-linear duration-1000 ${meetActivate ? "animateFullWidth" : "animateNormalWidth"}`}>
-            <a className='cursor-pointer'>V9 meet</a>
+    <div className="sticky z-50 top-0 left-0 transition-none dark:text-white ">
+      <div className="mx-20 px-6 py-4 flex justify-between">
+        <Link href="/">
+          <div
+            className={`font-semibold text-xl no-underline text-center w-20 transition ease-linear duration-1000 ${
+              meetActivate ? "animateFullWidth" : "animateNormalWidth"
+            }`}
+          >
+            <a className="cursor-pointer">V9 meet</a>
           </div>
-      </Link>
-      <div className="flex items-center">
-            <div className={`transition-none ${meetActivate ? "w-0 h-0 invisible" : ""}`}>
-            <Link href='/'>
+        </Link>
+        <div className="flex items-center">
+          <div
+            className={`transition-none ${
+              meetActivate ? "w-0 h-0 invisible" : ""
+            }`}
+          >
+            <Link href="/">
               <a className="mx-2">Home</a>
             </Link>
-            <Link href='https://www.vivek9patel.dev'>
+            <Link href="https://www.vivek9patel.com">
               <a className="mx-2">About Me</a>
             </Link>
-            </div>
-          <input onChange={toggleThemeMode} checked={themeMode === 'dark'} className="themeToggle mx-2" type="checkbox"></input>
+          </div>
+          <input
+            onChange={toggleThemeMode}
+            checked={themeMode === "dark"}
+            className="themeToggle mx-2"
+            type="checkbox"
+          ></input>
+        </div>
       </div>
-     </div>
-     <div className="w-full dark:bg-gray-200 bg-black h-1">
-      <div className={`bg-v9-pink w-0 h-1 ${loader ? "triggerLoader" : ""}`}></div>
+      <div className="w-full dark:bg-gray-200 bg-black h-1">
+        <div
+          className={`bg-v9-pink w-0 h-1 ${loader ? "triggerLoader" : ""}`}
+        ></div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- components/meta/MetaTags.tsx
+++ components/meta/MetaTags.tsx
@@ -1,31 +1,61 @@
-import React from 'react'
+import React from "react";
 
 export default function MetaTags() {
-    return (
-        <>
-            <link rel="apple-touch-icon" sizes="180x180" href="/meta/apple-touch-icon.png" />
-            <link rel="icon" type="image/png" sizes="32x32" href="/meta/favicon-32x32.png" />
-            <link rel="icon" type="image/png" sizes="16x16" href="/meta/favicon-16x16.png" />
-            <link rel="manifest" href="/meta/site.webmanifest" />
-            <link rel="mask-icon" href="/meta/safari-pinned-tab.svg" color="#5bbad5" />
-            <meta name="msapplication-TileColor" content="#2b5797" />
-            <meta name="theme-color" content="#162447" />
+  return (
+    <>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/meta/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/meta/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/meta/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/meta/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/meta/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <meta name="msapplication-TileColor" content="#2b5797" />
+      <meta name="theme-color" content="#162447" />
 
-            <title>V9 Meet</title>
-            <meta name="title" content="Chat with vivek9patel | V9 Meet" />
-            <meta name="description" content="vivek9patel's video conferencing application" />
+      <title>V9 Meet</title>
+      <meta name="title" content="Chat with vivek9patel | V9 Meet" />
+      <meta
+        name="description"
+        content="vivek9patel's video conferencing application"
+      />
 
-            <meta property="og:type" content="website" />
-            <meta property="og:url" content="https://meet.vivek9patel.dev/" />
-            <meta property="og:title" content="Chat with vivek9patel | V9 Meet" />
-            <meta property="og:description" content="vivek9patel's video conferencing application" />
-            <meta property="og:image" content="/meta/meta-social-image.png" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://meet.vivek9patel.com/" />
+      <meta property="og:title" content="Chat with vivek9patel | V9 Meet" />
+      <meta
+        property="og:description"
+        content="vivek9patel's video conferencing application"
+      />
+      <meta property="og:image" content="/meta/meta-social-image.png" />
 
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:url" content="https://meet.vivek9patel.dev/" />
-            <meta property="twitter:title" content="Chat with vivek9patel | V9 Meet" />
-            <meta property="twitter:description" content="vivek9patel's video conferencing application" />
-            <meta property="twitter:image" content="/meta/meta-social-image.png" />
-        </>
-    )
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://meet.vivek9patel.com/" />
+      <meta
+        property="twitter:title"
+        content="Chat with vivek9patel | V9 Meet"
+      />
+      <meta
+        property="twitter:description"
+        content="vivek9patel's video conferencing application"
+      />
+      <meta property="twitter:image" content="/meta/meta-social-image.png" />
+    </>
+  );
 }
