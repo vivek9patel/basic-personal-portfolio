0be1aad16eb74338ca57e49cd8ffec0be8bab00a
Vivek Patel <vivek.p9737@gmail.com>
2023-06-10 15:00:25 -0700

    update meta tags

--- components/meta/MetaTags.tsx
+++ components/meta/MetaTags.tsx
@@ -29,34 +29,41 @@ export default function MetaTags() {
       <meta name="msapplication-TileColor" content="#2b5797" />
       <meta name="theme-color" content="#162447" />
 
-      <title>Vivek Patel's Portfolio</title>
-      <meta name="title" content="Vivek Patel's Personal Website | Blogs " />
+      <title>Vivek Patel</title>
+      <meta name="title" content="Vivek Patel" />
       <meta
         name="description"
-        content="vivek9patel's personal website with blogs on the topics of  React.js, CSS, website designing & making."
+        content="Vivek's Portfolio Website with his past experiences, projects, and resume."
       />
+      <meta name="author" content="Vivek Patel (vivek9patel)" />
+      <meta
+        name="keywords"
+        content="vivek9patel, vivek9patel portfolio, vivek patel protfolio,vivek patel computer science, vivek patel, vivek patel asu, vivek patel arizona state university, vivek patel basic portfolio"
+      />
+      <meta name="robots" content="index, follow" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="language" content="English" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
 
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://vivek9patel.com/" />
-      <meta
-        property="og:title"
-        content="Vivek Patel's Personal Website | Blogs "
-      />
+      <meta property="og:title" content="Vivek Patel" />
       <meta
         property="og:description"
-        content="vivek9patel's personal website with blogs on the topics of  React.js, CSS, website designing & making."
+        content="Vivek's Portfolio Website with his past experiences, projects, resume."
       />
       <meta property="og:image" content="/meta/meta-social-image.png" />
+      <meta name="og:locale" content="en_IN" />
+      <meta name="og:type" content="website" />
 
+      <meta name="twitter:site" content="vivek9patel" />
+      <meta name="twitter:creator" content="vivek9patel" />
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content="https://vivek9patel.com/" />
-      <meta
-        property="twitter:title"
-        content="Vivek Patel's Personal Website | Blogs "
-      />
+      <meta property="twitter:title" content="Vivek Patel" />
       <meta
         property="twitter:description"
-        content="vivek9patel's personal website with blogs on the topics of  React.js, CSS, website designing & making."
+        content="Vivek's Portfolio Website with his past experiences, projects, resume."
       />
       <meta property="twitter:image" content="/meta/meta-social-image.png" />
     </>
